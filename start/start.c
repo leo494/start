20b3f17de4b4defa2a2efe5c5d5ef2d1e91b9d91
leo <tmddls0lek@gmail.com>
2022-07-10 22:33:30 +0900

    상대오차가 뭐야

--- start/start.c
+++ start/start.c
@@ -1,6 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-	printf("hello world! \n");
-	return;
+	float a, b;
+	scanf("%f %f", &a, &b);
+	printf("%0.11f\n", (float)a / b);
+	return 0;
 }
